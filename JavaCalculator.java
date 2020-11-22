a2fbeff9173fde4466f2419069073bba01d3baa5
Khoimaina <khoibig@gmail.com>
2020-11-22 13:48:47 -0500

    java classes and objects homework 2

---
+++ JavaCalculator.java
@@ -0,0 +1,25 @@
+package pethomework2;
+
+public class JavaCalculator {
+	
+	public double add(int a, int b) {
+        return (double)a+b;
+}
+
+public double subtraction(int a, int b) {
+    return (double)a-b;
+}
+
+public  double multiplication(int a, int b) {
+    return (double)a*b;
+}
+
+public double division(int a, int b) {
+    return (double)a/b;
+}
+
+public double square(int a) {
+    return a*a;
+}
+
+}
